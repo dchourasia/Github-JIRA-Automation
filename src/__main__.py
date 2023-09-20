9b5add2cce579a1943156c85fe99c51cb56eb8d3
Deepak Chourasia <dchouras@redhat.com>
2023-09-20 15:05:18 +0530

    updating github-jira automation

--- src/__main__.py
+++ src/__main__.py
@@ -9,6 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import re, traceback
 from ghj_config import ghj_config
+from collections import defaultdict
 def env_opts(env: str):
     if env in os.environ:
         return {'default': os.environ[env]}
@@ -94,7 +95,7 @@ def fetch_prs(gh: Github,
 
 
 
-def fetch_issues(config:ghj_config, component:dict, commits_with_no_issue_ref:list, commits_directly_made_to_downstream:list):
+def fetch_issues(config:ghj_config, component:dict, commits_with_no_issue_ref:defaultdict(list), commits_without_pr:defaultdict(list), commits_directly_made_to_downstream:defaultdict(list)):
     """
     Fetch all Github Issues associated with the commits between previous_release and
     target_release tags. If 2 commits are associated with a single pr,
@@ -110,21 +111,32 @@ def fetch_issues(config:ghj_config, component:dict, commits_with_no_issue_ref:li
     :param labels_to_filter:
     :return:
     """
-    def filter_labels(label_name):
-        return label_name in config.labels_to_filter
 
-    def process_upstream_pr(upstream_PR:PullRequest, repo_issues:list, repo:str):
+    def get_issues_for_org(upstream_org:str, gh:Github):
+        issues = []
+        issues = gh.search_issues(query=f'is:issue user:{upstream_org} label:{config.filter_labels}&per_page=100')
+
+
+
+
+
+    def process_upstream_pr(upstream_org, upstream_PR:PullRequest, repo_issues:list, repo:str):
         if upstream_PR.number not in upstream_repo_prs:
-            repo_issues += get_linked_issues(config.upstream_org, repo, upstream_PR, commits_with_no_issue_ref)
+            repo_issues += get_linked_issues(upstream_org, repo, upstream_PR, commits_with_no_issue_ref)
             upstream_repo_prs.append(upstream_PR.number)
 
     issues = []
-    for repo in component["repos"]:
+
+
+    for repo in component["cpaas_repos"]:
         downstream_repo_prs = []
         upstream_repo_prs = []
         repo_issues = []
-        downstream_repo = config.gh.get_organization(config.downstrean_org).get_repo(repo)
-        upstream_repo = config.gh.get_organization(config.upstream_org).get_repo(repo)
+        upstream_org, downstream_org = config.upstream_org, config.downstream_org
+        if '/' in repo:
+            upstream_org, repo = repo.split('/')[0], repo.split('/')[1]
+        downstream_repo = config.gh.get_organization(downstream_org).get_repo(repo)
+        upstream_repo = config.gh.get_organization(upstream_org).get_repo(repo)
         previous_commits = downstream_repo.get_commits(sha=config.previous_release, since=datetime.datetime.today() - datetime.timedelta(days=60))
         current_commits = downstream_repo.get_commits(sha=config.target_release, since=datetime.datetime.today() - datetime.timedelta(days=45))
 
@@ -135,21 +147,28 @@ def fetch_issues(config:ghj_config, component:dict, commits_with_no_issue_ref:li
             else:
                 break
         for downstream_commit in target_commits:
-            downstream_PR = downstream_commit.get_pulls().get_page(0).pop()
-            if get_github_org(downstream_PR.html_url) == config.upstream_org:
-                process_upstream_pr(downstream_PR, repo_issues, repo)
-            elif downstream_PR.number not in downstream_repo_prs:
-                upstream_commits = downstream_PR.get_commits()
-                for upstream_commit in upstream_commits:
-                    if upstream_commit in target_commits:
-                        commits_directly_made_to_downstream.append(upstream_commit)
-                        continue
-                    upstream_PR = upstream_commit.get_pulls().get_page(0).pop()
-                    process_upstream_pr(upstream_PR, repo_issues, repo)
-                downstream_repo_prs.append(downstream_PR.number)
+            try:
+                pull_requests = downstream_commit.get_pulls()
+                if pull_requests.totalCount > 0:
+                    downstream_PR = downstream_commit.get_pulls().get_page(0).pop()
+                    if get_github_org(downstream_PR.html_url) == upstream_org:
+                        process_upstream_pr(upstream_org, downstream_PR, repo_issues, repo)
+                    elif downstream_PR.number not in downstream_repo_prs:
+                        upstream_commits = downstream_PR.get_commits()
+                        for upstream_commit in upstream_commits:
+                            if upstream_commit in target_commits:
+                                commits_directly_made_to_downstream[repo].append(upstream_commit.html_url)
+                                continue
+                            upstream_PR = upstream_commit.get_pulls().get_page(0).pop()
+                            process_upstream_pr(upstream_org, upstream_PR, repo_issues, repo)
+                        downstream_repo_prs.append(downstream_PR.number)
+                else:
+                    commits_without_pr[repo].append(downstream_commit.html_url)
+            except Exception as e:
+                print(f'Exception while getting PR for the commit {downstream_commit.html_url}, skipping the commit')
+                print(e)
+                print(traceback.format_exc())
         repo_issues = list(set(repo_issues))
-        # repo_issues = [issue.split('/')[-1] for issue in repo_issues]
-        # repo_issues = [upstream_repo.get_issue(issue) for issue in repo_issues]
         issues += repo_issues
 
     return issues
@@ -157,7 +176,7 @@ def fetch_issues(config:ghj_config, component:dict, commits_with_no_issue_ref:li
 def get_github_org(url:str):
     return url.split('/')[3] if url else ''
 
-def get_linked_issues(upstream_org, repo, PR: PullRequest, commits_with_no_issue_ref:list):
+def get_linked_issues(upstream_org, repo, PR: PullRequest, commits_with_no_issue_ref:defaultdict(list)):
     issues = []
     try:
         pr_url = f"https://github.com/{upstream_org}/{repo}/pull/{PR.number}"
@@ -166,7 +185,7 @@ def get_linked_issues(upstream_org, repo, PR: PullRequest, commits_with_no_issue
         issueForm = soup.find("form", {"aria-label": re.compile('Link issues')})
         issues = [i["href"] for i in issueForm.find_all("a")]
         if not issues:
-            commits_with_no_issue_ref.append(pr_url)
+            commits_with_no_issue_ref[repo].append(pr_url)
     except Exception as e:
         print(traceback.format_exc())
 
@@ -182,7 +201,8 @@ def submit_jira(jc: JIRA, downstream_release: str, project: str, summary: str, d
         'issuetype': {'name': issuetype},
         'labels': labels,
         'priority': {'name': priority},
-        'components': [jira_component]
+        'components': [jira_component],
+        'customfield_12311240': downstream_release
     }
 
     new_issue = jc.create_issue(fields=issue_dict)
@@ -194,32 +214,14 @@ def parse_arguments():
         description="Create a Jira Issue from a GitHub tag release."
     )
 
-    # parser.add_argument("--component", dest="component", help="ODH Component name, used in Jira title.", required=False, default='Build and Release')
-    # parser.add_argument("--target_release", dest="target_release",
-    #                     help="Downstream Release target, should match Jira 'Target Release' field.", required=False, default='RHODS_1.33.0_GA')
-    # parser.add_argument("--org", dest="organization", help="Downstream GitHub Org", required=False, default='red-hat-data-services')
-    # parser.add_argument("--labels", dest="pr_filter_labels",
-    #                     help="Upstream labels used to select on the GH issues to include in target Jira issue."
-    #                          "Delimited by comma (,).",
-    #                     required=False, default='')
-    # parser.add_argument("--jira_server", dest="jira_server", help="Jira Server to connect to.", required=False, default='https://issues.redhat.com/')
-    # parser.add_argument("--jira_project", dest="jira_project", help="Jira Project", required=False, default='RHODS')
-    # parser.add_argument("--jira_labels", dest="jira_labels", help="Jira Labels to add to the Jira issue. "
-    #                                                               "Delimited by comma (,).", required=False, default='eng,groomed')
-    # parser.add_argument("--jira_issue_type", dest="jira_issue_type", help="Jira Issue Type (E.g. Story, Task, etc.)",
-    #                     required=False, default='Bug')
-    #
-    # parser.add_argument("--jira_priority", dest="jira_priority", help="Jira Priority, defaults to Normal.",
-    #                     default='Normal', required=False)
-    #
-    # parser.add_argument("--dev", dest="dev",
-    #                     action="store_true",
-    #                     help="Use this flag to store gh data in cache after first run. This will "
-    #                          "reduce the number of api calls made to the GH api in consecutive runs.", required=False)
-    #
+    parser.add_argument("--dev", dest="dev",
+                        action="store_true",
+                        help="Use this flag to store gh data in cache after first run. This will "
+                             "reduce the number of api calls made to the GH api in consecutive runs.", required=False)
+
     parser.add_argument("--config", dest="config",
                         help="A JSON config ]",
-                        **env_opts("CONFIG"), default="config/components.json")
+                        **env_opts("CONFIG"))
 
     parser.add_argument("--gh_token", dest="gh_token",
                         help="", **env_opts("GITHUB_TOKEN"))
@@ -230,42 +232,56 @@ def parse_arguments():
     args = parser.parse_args()
 
     return args
-
+def extract_issues_with_filter_labels(config:Github, filter_label_issues:defaultdict(list)):
+    issues = []
+    upstream_orgs = [config.upstream_org]
+    upstream_orgs += list(set([repo.split('/')[0] for component in config.components for repo in component["cpaas_repos"] + component["non_cpaas_repos"] if '/' in repo]))
+    for upstream_org in upstream_orgs:
+        issues += config.gh.search_issues(query=f'is:issue user:{upstream_org} label:{config.filter_labels}')
+    # component_repos = {component["component_name"]:list(set(component["cpaas_repos"] + component["non_cpaas_repos"])) for component in config.components}
+    repos_component = {(repo if '/' not in repo else repo.split('/')[1]):component["component_name"] for component in config.components for repo in list(set(component["cpaas_repos"] + component["non_cpaas_repos"]))}
+    for issue in issues:
+        if issue.repository.name in repos_component:
+            filter_label_issues[repos_component[issue.repository.name]].append(issue.html_url)
+        else:
+            filter_label_issues['Missing_Repos'].append(issue.html_url)
 
 def main():
     args = parse_arguments()
+
     config = ghj_config(args.config, args.gh_token, args.jira_token)
-    commits_with_no_issue_ref, commits_directly_made_to_downstream = [], []
+    commits_with_no_issue_ref, commits_without_pr, commits_directly_made_to_downstream, filter_label_issues = defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
+
+    extract_issues_with_filter_labels(config, filter_label_issues)
     for component in config.components:
         component_name = component["component_name"]
         jira_component = component["jira_component"]
-        repos = component["repos"]
-        if args.dev:
-            from src.util import cache_create, cache_fetch
-            if not os.path.exists("./cache"):
-                prs = fetch_prs(config.gh, repos, config.upstream_org, config.labels_to_filter)
-                cache_create(prs)
-            prs = cache_fetch(config.gh)
+        print(f'******************* Starting Component {component_name} *******************')
+        issues = fetch_issues(config, component, commits_with_no_issue_ref, commits_without_pr, commits_directly_made_to_downstream)
+        issues = list(set(issues + filter_label_issues[component["component_name"]]))
+        if issues:
+            msg = build_msg_issues(issues)
+            summary = "Github Issues for Component {0} for release {1}".format(component_name, config.target_release)
+            print(summary, msg)
+            new_jira = submit_jira(
+                jc=config.jc,
+                project=config.jira_project,
+                summary=summary,
+                description=msg,
+                issuetype=config.jira_issue_type,
+                labels=config.jira_labels,
+                priority=config.jira_priority,
+                downstream_release=config.jira_target_release,
+                jira_component=jira_component
+            )
+            print(new_jira)
         else:
-            # prs = fetch_prs(gh, repos, organization, labels_to_filter)
-            issues = fetch_issues(config, component, commits_with_no_issue_ref, commits_directly_made_to_downstream)
+            print('not enough github issues found for component {0} for release {1}'.format(component_name, config.target_release))
+    print('commits_with_no_issue_ref', commits_with_no_issue_ref)
+    print('commits_without_pr', commits_without_pr)
+    print('commits_directly_made_to_downstream', commits_directly_made_to_downstream)
 
-        msg = build_msg_issues(issues)
-        summary = "Github Issues for Component {0} for release {1}".format(component_name, config.target_release)
-        print(msg)
 
-        # new_jira = submit_jira(
-        #     jc=jc,
-        #     project=jira_project,
-        #     summary=summary,
-        #     description=msg,
-        #     issuetype=jira_issue_type,
-        #     labels=jira_labels,
-        #     priority=jira_priority,
-        #     downstream_release=target_release,
-        #       jira_component=jira_component
-        # )
-        # print(new_jira)
 
 if __name__ == "__main__":
     main()
--- src/ghj_config.py
+++ src/ghj_config.py
@@ -1,21 +1,25 @@
 import json
 from github import Auth, PullRequest, Github, Repository
 from jira import JIRA
+import os
 class ghj_config:
 
     def __init__(self, config_file, gh_token, jira_token):
-        config = json.load(config_file)
+        print(os.getcwd())
+        config = json.load(open(config_file))
         self.target_release = config["target_release"]
         self.previous_release = config["previous_release"]
         self.upstream_org = config["upstream_org"]
-        self.downstrean_org = config["downstrean_org"]
-        self.labels_to_filter = config["labels"]
+        self.downstream_org = config["downstream_org"]
+        self.filter_labels = config["filter_labels"]
 
         self.jira_server = config["jira_server"]
         self.jira_project = config["jira_project"]
         self.jira_issue_type = config["jira_issue_type"]
         self.jira_labels = config["jira_labels"]
         self.jira_priority = config["jira_priority"]
+        self.jira_target_release = config["jira_target_release"]
+
         self.components = config["components"]
         auth = Auth.Token(gh_token)
 
